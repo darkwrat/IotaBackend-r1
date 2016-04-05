93d0b89508669bb7f1d37933f32320f2fcc8e6fe
Aleksandr Petrukhin <shmeleine@gmail.com>
2016-04-05 23:38:20 +0400

    Fixed minor mistakes

--- src/main/java/ru/cdecl/pub/iota/servlets/ConcreteUserServlet.java
+++ src/main/java/ru/cdecl/pub/iota/servlets/ConcreteUserServlet.java
@@ -6,6 +6,7 @@ import co.paralleluniverse.fibers.Suspendable;
 import co.paralleluniverse.fibers.servlet.FiberHttpServlet;
 import org.jetbrains.annotations.Nullable;
 import org.json.JSONObject;
+import org.json.JSONTokener;
 import org.jvnet.hk2.annotations.Service;
 import ru.cdecl.pub.iota.services.AccountService;
 
@@ -15,7 +16,6 @@ import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
 import java.io.IOException;
 import ru.cdecl.pub.iota.models.UserProfile;
 
@@ -32,19 +32,19 @@ public final class ConcreteUserServlet extends FiberHttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         final Long userId = getUserIdFromHttpRequest(req);
         if (userId == null) {
-            makeInvalidResponse(resp);
+            makeErrorResponse(resp, RESP_STATUS_NOT_AUTHORIZED, "{}");
             return;
         }
 
         UserProfile profile = accountService.getUserProfile(userId);
-        //String email = profile.getEmail();
-        //String login = profile.getLogin();
-        //if (email.equals("") || login.equals("")) {
-        //    makeInvalidResponse(resp);
-        //    return;
-        //} //TODO uncomment when implemented(a.petrukhin).
+        final String email = profile.getEmail();
+        final String login = profile.getLogin();
+        if (!"".equals(email) || "".equals(login)) {
+            makeErrorResponse(resp, RESP_STATUS_FORBIDDEN, "Can not get user");
+            return;
+        }
 
-        JSONObject object = new JSONObject();
+        final JSONObject object = new JSONObject();
         object.put("id", userId);
         //object.put("login", login);
         //object.put("email", email);
@@ -57,12 +57,30 @@ public final class ConcreteUserServlet extends FiberHttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         final Long userId = getUserIdFromHttpRequest(req);
         if (userId == null) {
-            makeInvalidResponse(resp);
+            makeErrorResponse(resp, RESP_STATUS_NOT_AUTHORIZED, "{}");
             return;
         }
-        JSONObject obj = makeJSONFromRequest(req);
-        resp.setStatus(200);
-        resp.getWriter().write(obj.toString());
+        final JSONObject obj = makeJSONFromRequest(req);
+
+        if (!obj.has("login") || !obj.has("email") || !obj.has("password")) {
+            resp.setStatus(RESP_STATUS_SERVER_ERROR); //TODO: change status code (a.petrukhin).
+            resp.getWriter().write("Invalid JSON Request");
+            return;
+        }
+
+        String login = obj.get("login").toString();
+        UserProfile profile = accountService.getUserProfile(login);
+        //String profileLogin = profile.getLogin(); //TODO implement when ready(a.petrukhin).
+        if (!(login.equals("test"))) {
+            final JSONObject jsonResponse = new JSONObject();
+            jsonResponse.put("status", RESP_STATUS_FORBIDDEN);
+            jsonResponse.put("message", "Чужой юзер");
+            makeErrorResponse(resp, RESP_STATUS_FORBIDDEN, jsonResponse.toString());
+            return;
+        }
+
+        resp.setStatus(RESP_STATUS_OK);
+        resp.getWriter().write("test");
     }
 
     @Override
@@ -79,26 +97,22 @@ public final class ConcreteUserServlet extends FiberHttpServlet {
         try {
             return Long.parseLong(requestUri.substring(requestUri.lastIndexOf('/') + 1));
         } catch (NumberFormatException | IndexOutOfBoundsException e) {
-            System.out.println("Can not convert userID from String to Long in getUserIdFromHttpRequest in ConcreteUserServlet");
             return null;
         }
     }
 
-    private void makeInvalidResponse(HttpServletResponse resp) throws IOException, ServletException {
-        resp.setStatus(401);
-        resp.getWriter().write("{}");
+    private void makeErrorResponse(HttpServletResponse resp, int respCode, String message) throws IOException, ServletException {
+        resp.setStatus(respCode);
+        resp.getWriter().write(message);
     }
 
     private JSONObject makeJSONFromRequest(HttpServletRequest req) throws IOException {
-        StringBuilder sb = new StringBuilder();
-        BufferedReader reader = req.getReader();
-
-        String line;
-        while ((line = reader.readLine()) != null) {
-            sb.append(line).append('\n');
-        }
-
-        System.out.println(sb.toString());
-        return new JSONObject(sb.toString());
+        final JSONTokener tokener = new JSONTokener(req.getInputStream());
+        return new JSONObject(tokener);
     }
+
+    private static final int RESP_STATUS_OK = 200;
+    private static final int RESP_STATUS_NOT_AUTHORIZED = 401;
+    private static final int RESP_STATUS_FORBIDDEN = 403;
+    private static final int RESP_STATUS_SERVER_ERROR = 500;
 }
